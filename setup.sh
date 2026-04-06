44e0a65f885a5bcb6a1416b3362e54485e392922
localfont <83416665+localfont@users.noreply.github.com>
2026-04-06 21:04:05 +0300

    Revise setup.sh to mount filesystems and configure chroot
    Updated the setup script to mount necessary filesystems and copy resolv.conf.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-# Önce bağlı dizinleri güvenli bir şekilde ayır
-sudo umount -l /home/tc/debian-fs/dev/pts
-sudo umount -l /home/tc/debian-fs/dev
-sudo umount -l /home/tc/debian-fs/proc
-sudo umount -l /home/tc/debian-fs/sys
-sudo umount -l /home/tc/debian-fs/run
+#!/bin/bash
+export TARGET="/home/tc/debian-fs"
+
+sudo mount --bind /dev $TARGET/dev
+sudo mount --bind /dev/pts $TARGET/dev/pts
+sudo mount --bind /proc $TARGET/proc
+sudo mount --bind /sys $TARGET/sys
+sudo mount --bind /run $TARGET/run
+
+sudo cp /etc/resolv.conf $TARGET/etc/resolv.conf
+
+sudo chroot $TARGET /bin/bash
